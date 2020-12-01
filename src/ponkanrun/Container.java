d7362038bd2328eb4119c86d4fa68cc345064a85
LCano <leonardomacedocano@gmail.com>
2020-11-30 23:20:58 -0300

    add container

--- src/ponkanrun/Container.java
+++ src/ponkanrun/Container.java
@@ -1,5 +1,19 @@
 package ponkanrun;
 
-public class Container {
+import javax.swing.JFrame;
 
+public class Container extends JFrame {
+
+	public Container() {
+		setTitle("Ponkan Run");
+		setSize(1024, 728);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLocationRelativeTo(null);
+		this.setResizable(false);
+		setVisible(true);
+	}
+
+	public static void main (String []args) {
+		new Container();
+	}
 }
